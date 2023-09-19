df66606389a019da3146ccaefe1679a354b252e0
kalaniJ94 <kalaniJ94@gmail.com>
2023-09-18 21:20:37 -0600

    new seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,12 @@
-INSERT INTO movies (name)
+INSERT INTO movies (movie_name)
 VALUES ("Midsommar"),
         ("The VVitch"),
         ("Hereditary"),
         ("Talk to Me");
 
-   INSERT INTO reviews (name)
-VALUES ("It's pretty good"),
-        ("It's okay"),
-        ("I like it"),
-        ("So good!");
+   INSERT INTO reviews ( movie_id, review)
+VALUES (1, "It's pretty good"),
+        (2, "It's okay"),
+        (3, "I like it"),
+        (4, "So good!");
      
