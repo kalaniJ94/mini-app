7d48c47ad919e74ac47ec52b38395af26986eb63
kalaniJ94 <kalaniJ94@gmail.com>
2023-09-18 21:10:06 -0600

    Seeds

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO movies (name)
+VALUES ("Midsommar"),
+        ("The VVitch"),
+        ("Hereditary"),
+        ("Talk to Me");
+
+   INSERT INTO reviews (name)
+VALUES ("It's pretty good"),
+        ("It's okay"),
+        ("I like it"),
+        ("So good!");
+     
